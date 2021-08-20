5a285832b3cb5df60fbcfc19f13990af39f099cb
unknown <you@example.com>
2021-08-20 20:33:06 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,37 @@
+
+var objAdmin=[
+  {
+    username: "admin001",
+    password: "pass001"
+  },
+  {
+    username: "admin002",
+    password: "pass002"
+  },
+  {
+    username: "admin003",
+    password: "pass003"
+  },
+  {
+    username: "admin004",
+    password: "pass004"
+  }, 
+  {
+    username: "admin005",
+    password: "pass005"
+  }
+]
+
+ function getInfo(){
+    var username= document.getElementById("username").value;
+    var password= document.getElementById("password").value;
+
+   for(i=0 ;i<objAdmin.length;i++){
+    if(username == objAdmin[i].username && password == objAdmin[i].password){
+      alert("Welcome, "+username);
+      return location.replace("cart.html");
+    } 
+  }
+  alert("Wrong username or password");
+}
+
