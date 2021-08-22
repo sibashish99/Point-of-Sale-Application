05fcdb2fd42a5943b47d078b9673cfa43c1b9b10
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-08-22 15:02:02 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -2,17 +2,17 @@
 var objProduct=[
     {
       pid:"p001",
-      pname: "Tide Plus Double Power Lemon & Mint Detergent Powder",
+      pname: "Tide Plus Detergent Powder",
       pprice: 45
     },
     {
         pid:"p002",
-        pname: "Fortune (Cooking Oil For Healthier Heart) Rice Bran Oil (Jar)",
+        pname: "Fortune Rice Bran Oil (Jar)",
         pprice: 345
     },
     {
         pid:"p003",
-        pname: "Mother's Choice Kachi Ghani Mustard Oil (Bottle)",
+        pname: "Mother's Choice Kachi Ghani Mustard Oil",
         pprice: 142
     },
     {
@@ -57,25 +57,47 @@ function showInfo(){
       if(proId == objProduct[i].pid){
 
         //alert("Product "+objProduct[i].pname+", price "+objProduct[i].pprice+", total units "+proUnit +", total price "+proUnit*objProduct[i].pprice);
-        alert("New product added")
-        var a= document.getElementById("tpname");
-        a.innerHTML = objProduct[i].pname;
+        //alert("New product added")
+        // var a= document.getElementById("tpname");
+        // a.innerHTML = objProduct[i].pname;
     
-        var b= document.getElementById("tpid");
-        b.innerHTML = objProduct[i].pid;
+        // var b= document.getElementById("tpid");
+        // b.innerHTML = objProduct[i].pid;
 
-        var e= document.getElementById("tunit")
-        e.innerHTML = proUnit;
+        // var e= document.getElementById("tunit")
+        // e.innerHTML = proUnit;
     
-        var c= document.getElementById("tpprice");
-        c.innerHTML = objProduct[i].pprice;
+        // var c= document.getElementById("tpprice");
+        // c.innerHTML = objProduct[i].pprice;
     
         
-        var d= document.getElementById("tptotal");
-        d.innerHTML = proUnit*objProduct[i].pprice;
+        // var d= document.getElementById("tptotal");
+        // d.innerHTML = proUnit*objProduct[i].pprice;
         
-        var f= document.getElementById("totalPrice");
-        f.innerHTML = proUnit*objProduct[i].pprice;
+        // var f= document.getElementById("totalPrice");
+        // f.innerHTML = proUnit*objProduct[i].pprice;
+
+
+        var arr = [ objProduct[i].pname , objProduct[i].pid , proUnit , objProduct[i].pprice , proUnit*objProduct[i].pprice];
+        var cont = document.getElementById('container-cart-main');
+  
+        // create ul element and set the attributes.
+        var ul = document.createElement('ul');
+        ul.setAttribute('style', 'padding: 5px; margin: 0; position:relative; top:10px; left:180px;');
+        ul.setAttribute('id', 'theList');
+  
+        for (i = 0; i <= arr.length - 1; i++) {
+          var li = document.createElement('li');  // create li element.
+          li.innerHTML = arr[i];     
+          li.setAttribute('style', 'display: inline; margin-right: 180px;');   
+  
+          ul.appendChild(li); // append li to ul.
+        }
+  
+        cont.appendChild(ul); // add list to the container.
+
+
+
         temp=1;
       } 
     }
