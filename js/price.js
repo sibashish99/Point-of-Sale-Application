1887dd1cb7fe48f74222da79cf2e82cf11242831
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-08-24 22:03:26 +0530

    Delete price.js

--- js/price.js
+++
@@ -1,8 +0,0 @@
-//implement newInfo() To get Bil number and time into the page
-
-function newInfo(){
-    document.getElementById("demo").innerHTML = Math.floor((Math.random()*100 + 1)*51226);
-    document.getElementById("demo1").innerHTML = new Date();
-}
-window.onload = newInfo;
-
