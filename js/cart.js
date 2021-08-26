1aabc3cf9c66e552fb6d5d818e7dd503a62d8788
Sibashish Biswas <62691250+sibashish99@users.noreply.github.com>
2021-08-26 11:49:41 +0530

    Update cart.js

--- js/cart.js
+++ js/cart.js
@@ -55,21 +55,20 @@ var objProduct=[
 
 //implementing the showInfo() function for display the product info
 /*first will iterate through the objProduct object if the proId matches the pid of the product then store information
-as array and treverse the array to put data into cart.js*/
-
+as array and traverse the array to put data into cart.html*/
 function showInfo(){
+    
     var proId= document.getElementById("proId").value;
     var proUnit= document.getElementById("pUnit").value;
-    var temp=0;
+    
     for(i=0 ;i<objProduct.length;i++){
       if(proId == objProduct[i].pid){
 
-        //alert("Product "+objProduct[i].pname+", price "+objProduct[i].pprice+", total units "+proUnit +", total price "+proUnit*objProduct[i].pprice);
+        
         //alert("New product added")
-
         var arr = [ objProduct[i].pname , objProduct[i].pid , proUnit , objProduct[i].pprice , proUnit*objProduct[i].pprice];
+     
         var cont = document.getElementById('container-cart-main');
-  
         // create ul element and set the attributes.
         var ul = document.createElement('ul');
         ul.setAttribute('style', 'padding: 5px; margin: 0; position:relative; top:10px; left:180px;');
@@ -81,14 +80,17 @@ function showInfo(){
           li.setAttribute('style', 'display: inline; margin-right: 180px;');   
   
           ul.appendChild(li); // append li to ul.
+
         }
-  
-        cont.appendChild(ul); // add list to the container.
-
-        temp=1;
-
-         allData( objProduct[i].pname , objProduct[i].pid , proUnit , objProduct[i].pprice , proUnit*objProduct[i].pprice);
+        cont.appendChild(ul); // add list to the containe
+        
        
+        temp=1;
+       
+        localStorage.setItem('id',arr[1]);
+        localStorage.setItem('price', arr[3]);
+        localStorage.setItem('unit', arr[2]);   
+        localStorage.setItem('total', arr[4]);
       } 
     }
     if(temp==0){
@@ -99,17 +101,9 @@ function showInfo(){
 
 //implement completeShop() function for complete the shopping and redirect to the price.html page
 function completeShop(){
-    var myWindow = window.open("price.html", "", "width=1000,height=1000");
+  window.open("price.html", "", "width=1000,height=1000");
 }
 
-//implement newInfo() To get Bil number and time into the page
-
-function newInfo(){
-    document.getElementById("demo").innerHTML = Math.floor((Math.random()*100 + 1)*51226);
-    document.getElementById("demo1").innerHTML = new Date();
-    allData(); 
-}
-window.onload = newInfo;
 
 
 
@@ -117,35 +111,10 @@ window.onload = newInfo;
 
 
 
-function allData(a,b,c,d,e){
 
-    // var proName = document.getElementById("pName").value;
-    // var proId = document.getElementById("proId").value;
-    // var proUnit = document.getElementById("pUnit").value;
-    // var proPrice = document.getElementById("pPrice").value;
-    // var proTotal = document.getElementById("pTotal").value;
+
 
     
-    var proName = a;
-    var proId =b;
-    var proUnit =c; 
-    var proPrice = d;
-    var proTotal = e;
+   
 
-    var arr = [ proName , proId , proUnit , proPrice , proTotal];
-    var cont = document.getElementById('container-cart-main-price');
-    var ul = document.createElement('ul');
-    ul.setAttribute('style', 'padding: 5px; margin: 0; position:relative; top:130px; left:100px;');
-    ul.setAttribute('id', 'theList');
-
-    for (i = 0; i <= arr.length; i++) {
-        var li = document.createElement('li');  // create li element.
-        li.innerHTML = arr[i];     
-        li.setAttribute('style', 'display: inline; margin-right: 180px;');   
-
-        ul.appendChild(li); // append li to ul.
-    }
-
-    cont.appendChild(ul); // add list to the container.
-}
        
